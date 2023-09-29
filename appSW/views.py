9ad2fd44c64878429a172132ac24e20dac4502e0
nogpedroc <146397247+nogpedroc@users.noreply.github.com>
2023-09-29 03:22:22 +0000

    Added Accounts Authentifications features (register, login, logout)

--- appSW/views.py
+++ appSW/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from .models import  SequelsPros, Chapters, TableInfo
 from .forms import chaptersForm, sequelsProsForm
 
+@login_required
 def homePage(request):
   sequelsPros = SequelsPros.objects.all()
   chapters = Chapters.objects.all()
   tableInfo = TableInfo.objects.all()
   return render(request, "homePage.html", context={'sequelsPros':sequelsPros,'chapters':chapters,'tableInfo':tableInfo})
 
+@login_required
 def sequelsProsPage(request):
   sequelsPros = SequelsPros.objects.all()
   return render(request, "sequelsPros.html", context={'sequelsPros':sequelsPros})
 
+@login_required
 def chaptersPage(request):
   chapters = Chapters.objects.all()
   return render(request, "chapters.html", context={'chapters':chapters})
 
+@login_required
 def add_sequelsPros(request):
   if request.method == 'GET':
     form = sequelsProsForm()
@@ -27,6 +34,7 @@ def add_sequelsPros(request):
       return redirect('sequelsProsPage')
     return render(request, "sequelsProsForm.html", context={'form':form,'action':'Adicionar'})
 
+@login_required
 def add_chapters(request):
   if request.method == 'GET':
     form = chaptersForm()
@@ -38,6 +46,7 @@ def add_chapters(request):
       return redirect('chaptersPage')
     return render(request, "chaptersForm.html", context={'form':form,'action':'Adicionar'})
 
+@login_required
 def update_sequelsPros(request, sequelsPros_id):
   if request.method == 'GET':
     sequelsPros = SequelsPros.objects.all()
@@ -52,6 +61,7 @@ def update_sequelsPros(request, sequelsPros_id):
       return redirect('sequelsProsPage')
     return render(request, "sequelsProsForm.html", context={'form':form,'sequelsPros':sequelsPros,'action':'Atualizar'})
 
+@login_required
 def update_chapters(request, chapters_id):
   if request.method == 'GET':
     chapters = Chapters.objects.all()
@@ -66,6 +76,7 @@ def update_chapters(request, chapters_id):
       return redirect('chaptersPage')
     return render(request, "chaptersForm.html", context={'form':form,'chapters':chapters,'action':'Atualizar'})
 
+@login_required
 def delete_sequelsPros(request, sequelsPros_id):
   sequelsPro = SequelsPros.objects.filter(id=sequelsPros_id).first()
   if request.method == 'POST':
@@ -74,6 +85,7 @@ def delete_sequelsPros(request, sequelsPros_id):
     return redirect('sequelsProsPage')
   return render(request, 'del_sequelsPros.html', context={'sequelsPro':sequelsPro})
 
+@login_required
 def delete_chapters(request, chapters_id):
   chapter = Chapters.objects.filter(id=chapters_id).first()
   if request.method == 'POST':
@@ -81,3 +93,25 @@ def delete_chapters(request, chapters_id):
       chapter.delete()
     return redirect('chaptersPage')
   return render(request, 'del_chapters.html', context={'chapter':chapter})
+
+def register_user(request):
+  if request.method == "POST":
+    user = User.objects.create_user(
+        request.POST['username'], 
+        request.POST['email'], 
+        request.POST['password']
+    )
+    user.save()
+    return(redirect('homePage'))
+  return render(request, 'register.html', context={"action":"Adicionar"})
+
+def confirm_logout(request):
+  if request.method == "POST":
+    if 'Y' in request.POST:
+      return(redirect('logout_user'))
+    return redirect('homePage')
+  return render(request, 'logout.html')
+
+def logout_user(request):
+  logout(request)
+  return redirect('/accounts/login/')
--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+LOGIN_URL = '/accounts/login/'
+
+LOGOUT_URL = '/accounts/logout/'
